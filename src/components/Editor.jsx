38fca3b1984e12e490602baa9a8dc6cba4067723
GalieoTrover <valamaskey@tutamail.com>
2023-04-23 15:06:41 +0530

    fixed desktop layout

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -6,9 +6,11 @@ function Editor({ currentNote, updateNote }) {
     return (
     <div className="container">
         <MDEditor value={currentNote.body} onChange={(value)=>updateNote(value)} tabSize={4} preview="edit" extraCommands={[commands.codeEdit]} />
-        <h5 className="preview-heading">Preview</h5>
-        <MDEditor.Markdown source={currentNote.body} style={{ whiteSpace: 'pre-wrap' }}/>
-    </div>
+        <div className="preview-window">
+            <h5 className="preview-heading">Preview</h5>
+            <MDEditor.Markdown source={currentNote.body} style={{ whiteSpace: 'pre-wrap' }}/>
+        </div>
+        </div>
     )
 }
 
